5dbcf95a02b0942cbf1aff88f258c61acb2dfbc1
turgun3 <44668211+turgun3@users.noreply.github.com>
2020-12-21 18:48:41 +0600

    Add files via upload

---
+++ lab.py
@@ -0,0 +1,36 @@
+
+from math import floor
+import random
+
+class Character(object):
+	level = 8
+	strength = 18 
+	dexterity = 12
+	constitution = 14
+	intelligence = 8
+	wisdom = 10
+	charisma = 8
+	max_hp = 10 + 
+	hp = 
+	armour_class = 
+	initiative
+
+	def attack(self):
+
+	def save_throw(self, attribute):
+
+    @abstractmethod
+    def perk(self):
+    	
+class Dragon(object):
+	level = 10
+	strength = 20 
+	dexterity = 10
+	constitution = 16
+	intelligence = 8
+	wisdom = 20
+	charisma = 8
+	max_hp = 10 + 
+	hp = 
+	armour_class = 
+	initiative	
